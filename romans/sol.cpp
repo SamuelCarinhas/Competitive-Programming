68442b8c31e79ce3ae583121e613925b3c5aec49
Samuel Carinhas <samuelsantos.c.2001@gmail.com>
2021-03-20 13:26:34 +0000

    Kattis: Romans

---
+++ romans/sol.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+
+	double x;
+	cin >> x;
+
+	unsigned long long sol = round(1000*x*5280/4854);
+	cout << sol << endl;
+
+	return 0;
+}
